414772d15fb186439106173e2cf99a905f8af451
David <davidbrown3142@gmail.com>
2020-09-01 17:59:29 -0400

    laying out question object, html

---
+++ Assets/Javascript/script.js
@@ -0,0 +1,53 @@
+var questions = [
+    {
+        name: "Question 1",
+        question: "Which function returns a random integer from min to max inclusively?",
+        choice1: "var getRandomInt = function(min, max) { return Math.floor(Math.random() * (max - min) + min + 1; }",
+        choice2: "function getRandomInt(min, max) { return Math.random(min, max) }",
+        choice3: "const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);",
+        choice4: "let getRandomInt = (min, max) => return Math.floor(Math.random() * (max - min) + min + 1);",
+        choice5: "None of the above.",
+        correct: "choice3"
+    },
+    {
+        name: "Question 2",
+        question: "Which of these is not a valid array method?",
+        choice1: ".pop()",
+        choice2: ".shift()",
+        choice3: ".toString()",
+        choice4: ".forEach()",
+        choice5: ".unShift",
+        correct: "choice5"
+    },
+    {
+        name: "Question 3",
+        question: "The Boolean() function tests whether a parameter is true or false. Which choice will return false?",
+        choice1: "Boolean(true)",
+        choice2: "Boolean(\"false\")",
+        choice3: "Boolean(0)",
+        choice4: "Boolean(1337)",
+        choice5: "None of the above.",
+        correct: "choice3"
+    },
+    {
+        name: "Question 4",
+        question: "What will typeof [] return?",
+        choice1: '"string"',
+        choice2: '"undefined"',
+        choice3: '"object"',
+        choice4: '"array"',
+        choice5: '"boolean"',
+        correct: "choice3"
+    },
+    {
+        name: "Question 5",
+        question: "Q5",
+        choice1: "ch1",
+        choice2: "ch2",
+        choice3: "ch3",
+        choice4: "ch4",
+        choice5: "None of the above.",
+        correct: "choice2"
+    }
+];
+
